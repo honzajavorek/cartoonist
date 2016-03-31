65433a095651ae067ddff3a664056ddce5673237
Ondrej Skopek <skopekondrej@gmail.com>
2016-03-31 12:23:39 +0200

    Added fedora to install script

--- install.sh
+++ install.sh
@@ -8,7 +8,12 @@ elif grep '^ID=.*opensuse' /etc/os-release > /dev/null; then
 elif grep '^ID=arch' /etc/os-release > /dev/null; then
   sudo pacman -S --needed $(cat ./dependencies/os-linux.txt)
 elif grep '^ID=.*fedora' /etc/os-release > /dev/null; then
-  sudo dnf install $(cat ./dependencies/os-fedora.txt)
+  # since Fedora 22, DNF is the default package manager
+  if command -v dnf >/dev/null 2>&1; then
+    sudo dnf install $(cat ./dependencies/os-fedora.txt)
+  else
+    sudo yum install $(cat ./dependencies/os-fedora.txt)
+  fi
 else
   echo 'Unsupported OS.'
   exit 1
